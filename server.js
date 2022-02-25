9df7b962edd78eba5ef9641b255b9f9bb4860ed0
Ronald K <2fixelectronics777@gmail.com>
2022-02-24 18:24:30 -0600

    First Commit wierd

---
+++ server.js
@@ -0,0 +1,31 @@
+﻿
+// Name:   Ronald Kiefer
+// Hebrew name:    ר ו נ  א ל ד    
+// Date:   February 24, 2022 Thursday
+// Description:  bread App for Boot Camp Back End 
+// Description:  Module 3 code along
+
+
+// DEPENDENCIES
+const express = require('express')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Welcome to an Awesome App about Breads')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+// LISTEN
+app.listen(PORT, () => {
+    console.log('nomming at port', PORT);
+})
+
+
