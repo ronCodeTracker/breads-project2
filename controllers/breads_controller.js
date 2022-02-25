fd9d3f78036d21fe2bf8f2d8c4a26871029648e5
Ronald K <2fixelectronics777@gmail.com>
2022-02-25 07:18:49 -0600

    update for codealong prt 2

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,10 +7,19 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+        {
+            breads: Bread
+        }
+    )
+    // res.send(Bread)
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
-})
+  })
 
 module.exports = breads
 
