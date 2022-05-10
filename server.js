a17184f1251e18e571605a6ec1b6e9013f4849e3
unknown <2fixelectronics777@gmail.com>
2022-05-10 18:57:08 -0500

    add change mongoose

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT
 
 //Mongoose ODM
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+mongoose.connect("mongodb+srv://new-user35:VccVccpw@clusterbootc.3unew.mongodb.net/breadcrud", { useNewUrlParser: true, useUnifiedTopology: true },
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
